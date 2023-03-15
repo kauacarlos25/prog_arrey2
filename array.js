8dc067038dfc5d19f4bde41078baf9dd8f6389d9
Kaua <letesk@gmail.com>
2023-03-15 09:19:43 -0300

    Final

--- array.js
+++ array.js
@@ -2,12 +2,12 @@ let valor_cidade = [] //variavel global
 
 
 function Inserir(){
-    let cidade = document.getElementById('cidade')
+    let cidade = document.getElementById('cidade').value
     valor_cidade.push(cidade)
-    console.log(cidade)
+    console.log(valor_cidade)
 }
 
 function Excluir(){
     valor_cidade.pop()
-    console.log(cidade)
+    console.log(valor_cidade)
 }
